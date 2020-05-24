97cf7b3f47c01a0e85d2f052889ded6977e639c2
Jacek <muszynskijk@gmail.com>
2020-05-24 20:10:27 +0200

    section about deafalt layout finished

--- src/components/sections/2about.js
+++ src/components/sections/2about.js
@@ -1,131 +1,157 @@
 import React from "react"
 import styled from "styled-components"
-import Image from "gatsby-image"
+import BackgroundImage from "gatsby-background-image"
 
 const Section = styled.section`
-  min-height: 100vh;
   width: 100%;
-  background-color: white;
   z-index: 20;
   position: relative;
   margin-top: 100vh;
-  color: white;
+  color: black;
+  height: 100%;
+  /* &::after {
+    content: "";
+    display: block;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    z-index: 21;
+    top: 0;
+    background-color: rgba(256, 256, 256, 0.2);
+  } */
 `
 
-const StyledImage = styled(Image)`
-  object-fit: cover;
-  z-index: 0;
-  opacity: 0.7;
-  height: 200vh;
-  opacity: 0.2;
+const StyledBackgroundImage = styled(BackgroundImage)`
+  width: 100%;
+  background-position: bottom center;
+  background-repeat: no-repeat;
+  background-size: cover;
 `
 
 const SectionContent = styled.div`
-  position: absolute;
-  top: 0;
-  color: black;
-`
-
-const SectionTitle = styled.h2`
-  display: block;
   position: relative;
-  margin-left: 10%;
-  font-size: 2.5rem;
-  width: 50%;
-  padding: 0;
-  &:before {
-    content: "";
-    display: block;
-    position: absolute;
-    height: 5px;
+  z-index: 31;
+  top: 0;
+  color: white;
+  padding: 0 10%;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  overflow: hidden;
+  background-color: rgba(0, 0, 0, 0.6);
+  .article {
     width: 100%;
-    background-color: yellow;
-    bottom: 0px;
-    transform: translateX(-20%);
-  }
-`
-
-const ARTICLE_1 = styled.div`
-  width: 84%;
-  padding: 7%;
-  font-size: 1.2rem;
-  line-height: 1.5rem;
-  font-weight: bold;
-  ul {
-    li {
-      list-style: none;
-      margin-top: 8px;
-      margin-bottom: 2px;
-      &::after {
+    .text {
+      font-size: 1.2rem;
+      line-height: 1.5rem;
+      font-weight: normal;
+      text-align: justify;
+      .last-acapit {
+        padding-left: 5px;
+        border-left: 5px solid red;
+      }
+    }
+    .article__title {
+      display: block;
+      position: relative;
+      font-size: 2.2rem;
+      width: 100%;
+      &:before {
         content: "";
         display: block;
-        width: 40px;
-        height: 2px;
-        background-color: black;
-        transform: translateX(-10px);
+        position: absolute;
+        height: 5px;
+        width: 50vw;
+        background-color: yellow;
+        bottom: -15px;
+        border-bottom: 2px solid black;
+      }
+    }
+    ul {
+      li {
+        list-style: none;
+        margin-top: 8px;
+        margin-bottom: 2px;
+        &::after {
+          content: "";
+          display: block;
+          width: 40px;
+          height: 2px;
+          background-color: yellow;
+          transform: translateX(-10px);
+        }
+      }
+    }
+  }
+  .article-one,
+  .article-three {
+    .article__title {
+      &:before {
+        transform: translateX(-20%);
+      }
+    }
+  }
+  .article-two {
+    .article__title {
+      text-align: right;
+      &:before {
+        transform: translateX(40vw);
       }
     }
   }
 `
 
-const ARTICLE_2 = styled.div`
-  position: relative;
-  .article__title {
-    margin-left: 40%;
-    text-align: left;
-    display: block;
-    position: relative;
-    margin-right: 10%;
-    font-size: 2.2rem;
-    width: 70%;
-    padding: 0;
-    &:before {
-      content: "";
-      display: block;
-      position: absolute;
-      height: 5px;
-      width: 100%;
-      background-color: yellow;
-      bottom: -5px;
-      transform: translateX(-10%);
-    }
-  }
-`
-
 const sectionAbout = props => {
+  console.log(props)
   return (
-    <Section>
-      <StyledImage fluid={props.fluid} />
-      <SectionContent id="about" className="content">
-        <SectionTitle>O nas</SectionTitle>
-        <ARTICLE_1>
-          {" "}
-          Biuro projektowe „Tech-Drog” Arkadiusz Pydzik powstało 2008 roku.
-          Świadczymy usługi w zakresie projektowania infrastruktury drogowej dla
-          inwestorów prywatnych jak i publicznych. Realizujemy zlecenia na
-          terenie województwa mazowieckiego jak i łódzkiego. Firma współpracuje
-          z wykwalifikowanymi specjalistami z branży drogowej jak i branż
-          infrastruktury towarzyszącej:{" "}
-          <ul>
-            <li> wodno-kanalizacyjnej,</li>
-            <li> energetycznej,</li>
-            <li> teletechnicznej,</li>
-            <li> gazowej.</li>
-          </ul>
-        </ARTICLE_1>
-        <ARTICLE_2 id="services">
-          <h3 className="article__title">Nasze usługi</h3>
-          <p>
-            świadczy kompleksowe usługi w zakresie wykonania dokumentacji
-            projektowej dla: dróg i ulic wraz z infrastrukturą towarzyszącą;
-            zjazdów indywidualnych jak i publicznych z dróg każdej kategorii;
-            parkingów i zatok postojowych; chodników i ścieżek rowerowych.
-            Ponadto posiadamy bogate doświadczenie w przygotowaniu projektów
-            stałej i tymczasowej organizacji ruchu wraz ze wszystkimi
-            niezbędnymi uzgodnieniami. Zapewniamy też nadzory inwestorskie.
-          </p>
-        </ARTICLE_2>
-      </SectionContent>
+    <Section image={props.imageUrl}>
+      <StyledBackgroundImage fluid={props.fluid}>
+        <SectionContent id="about" className="content">
+          <article className="article article-one">
+            <h3 className="article__title">O nas</h3>
+            <p className="text">
+              Biuro projektowe „Tech-Drog” Arkadiusz Pydzik powstało 2008 roku.
+              Świadczymy usługi w zakresie projektowania infrastruktury drogowej
+              dla inwestorów prywatnych jak i publicznych. Realizujemy zlecenia
+              na terenie województwa mazowieckiego jak i łódzkiego. Firma
+              współpracuje z wykwalifikowanymi specjalistami z branży drogowej
+              jak i branż infrastruktury towarzyszącej:{" "}
+              <ul>
+                <li> wodno-kanalizacyjnej,</li>
+                <li> energetycznej,</li>
+                <li> teletechnicznej,</li>
+                <li> gazowej.</li>
+              </ul>
+            </p>
+          </article>
+
+          <article id="services" className="article article-two">
+            <h3 className=" article__title">Nasze usługi</h3>
+            <p className="text">
+              Świadczy kompleksowe usługi w zakresie wykonania dokumentacji
+              projektowej dla: dróg i ulic wraz z infrastrukturą towarzyszącą;
+              zjazdów indywidualnych jak i publicznych z dróg każdej kategorii;
+              parkingów i zatok postojowych; chodników i ścieżek rowerowych.
+              Ponadto posiadamy bogate doświadczenie w przygotowaniu projektów
+              stałej i tymczasowej organizacji ruchu wraz ze wszystkimi
+              niezbędnymi uzgodnieniami. Zapewniamy też nadzory inwestorskie.
+            </p>
+          </article>
+          <article id="services" className="article article-three">
+            <h3 className="article__title">Ponadto</h3>
+            <p className="text">
+              Ponadto posiadamy bogate doświadczenie w przygotowaniu projektów
+              stałej i tymczasowej organizacji ruchu wraz ze wszystkimi
+              niezbędnymi uzgodnieniami.
+              <br />
+              <br />
+              <div className="last-acapit">
+                Zapewniamy również nadzory inwestorskie.
+              </div>
+            </p>
+          </article>
+        </SectionContent>
+      </StyledBackgroundImage>
     </Section>
   )
 }
--- src/components/sections/5contact.js
+++ src/components/sections/5contact.js
@@ -1,14 +1,26 @@
 import React from "react"
 import styled from "styled-components"
+
+import VisibilitySensor from "react-visibility-sensor"
+
 const Section = styled.section`
   min-height: 100vh;
   width: 100%;
 `
 
+function onChange(isVisible) {
+  console.log("Element is now %s", isVisible ? "visible" : "hidden")
+}
+
 const sectionContact = () => {
   return (
     <div id="contact">
-      <Section>contact</Section>
+      <Section>
+        contact
+        <VisibilitySensor onChange={onChange}>
+          <div>...content goes here...</div>
+        </VisibilitySensor>
+      </Section>
     </div>
   )
 }
