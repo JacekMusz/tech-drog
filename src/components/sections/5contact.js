2fda8472c90e1732a2fbb48e7327ddd5e70cb32d
Jacek <muszynskijk@gmail.com>
2020-05-26 21:24:09 +0200

    formik and from skeleton added

--- src/components/sections/5contact.js
+++ src/components/sections/5contact.js
@@ -1,5 +1,6 @@
 import React from "react"
 import styled from "styled-components"
+import { Formik } from "formik"
 
 const Section = styled.section`
   min-height: 150vh;
@@ -16,56 +17,123 @@ const ContactFormWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  .contact-form__input-wrapper {
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    margin: 10px;
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    .form {
-      display: flex;
-      justify-content: center;
-      flex-direction: column;
-    }
-    input {
-      background-color: transparent;
-      border: none;
-      border-bottom: 1px solid #888;
-      margin: 10px;
-      color: white;
-      outline: none;
-      cursor: pointer;
-      &:focus {
-        color: yellow;
-        border-bottom: 1px solid yellow;
-      }
-    }
+`
+
+const StyledInput = styled.input`
+  margin-left: 10px;
+  display: block;
+  border: 2px solid black;
+  background: black;
+  font-size: 20px;
+  color: white;
+  height: ${({ as }) => (as ? "200px" : "auto")};
+  width: ${({ as }) => (as ? "40vw" : "25vw")};
+  min-width: 200px;
+  margin-bottom: ${({ as }) => as && "40px"};
+  border-bottom: 1px solid gray;
+  &:focus {
+    border-bottom: 1px solid yellow;
+    outline: none;
+    color: yellow;
+  }
+  @media (min-width: 410px) {
+    height: ${({ as }) => (as ? "300px" : "auto")};
+  }
+  @media (min-width: 767px) {
+    height: ${({ as }) => (as ? "400px" : "auto")};
+  }
+  @media (min-width: 1199px) {
+    height: ${({ as }) => (as ? "500px" : "auto")};
   }
 `
+
+const StyledLabel = styled.label`
+  margin: 30px 10px 10px;
+  display: block;
+  font-size: 14px;
+  font-weight: bold;
+`
+
+const Button = styled.button`
+  margin-left: 10px;
+  border: 1px solid grey;
+  padding: 5px;
+  background-color: transparent;
+  color: white;
+  margin-bottom: 50px;
+  min-width: 150px;
+  cursor: pointer;
+  transition: 0.5s;
+  &:hover {
+    color: yellow;
+    border: 1px solid yellow;
+    background-color: black;
+  }
+  @media (min-width: 767px) {
+    width: 200px;
+  }
+`
+
 const sectionContact = () => {
   return (
     <div id="contact">
       <Section>
         <ContactFormWrapper>
           <h2>Czekamy na wiadomość od Ciebie</h2>
-          <form className="form" method="post" action="#">
-            <label>
-              Name
-              <input type="text" name="name" id="name" />
-            </label>
-            <label>
-              Email
-              <input type="email" name="email" id="email" />
-            </label>
-            <label>
-              Message
-              <textarea name="message" id="message" rows="15" />
-            </label>
-            <button type="submit">Send</button>
-            <input type="reset" value="Clear" />
-          </form>
+          <Formik
+            initialValues={{ name: "", email: "", message: "" }}
+            // validate={values => {
+            //   const errors = {}
+            //   if (!values.email) {
+            //     errors.email = "Required"
+            //   } else if (
+            //     !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+            //   ) {
+            //     errors.email = "Invalid email address"
+            //   }
+            //   return errors
+            // }}
+            onSubmit={(values, { setSubmitting }) => {
+              setTimeout(() => {
+                alert(JSON.stringify(values, null, 2))
+                setSubmitting(false)
+              }, 400)
+            }}
+          >
+            {({ values, handleChange, handleBlur, handleSubmit }) => (
+              <form onSubmit={handleSubmit}>
+                <StyledLabel htmlFor="name">Name</StyledLabel>
+                <StyledInput
+                  id="name"
+                  type="text"
+                  name="name"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.name}
+                />
+                <StyledLabel htmlFor="e-mail">E-mail</StyledLabel>
+                <StyledInput
+                  id="email"
+                  type="e-mail"
+                  name="email "
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.email}
+                />
+                <StyledLabel>Message</StyledLabel>
+                <StyledInput
+                  as="textarea"
+                  type="text"
+                  name="message"
+                  id="message"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.message}
+                />
+                <Button>Send message</Button>
+              </form>
+            )}
+          </Formik>
         </ContactFormWrapper>
       </Section>
     </div>
